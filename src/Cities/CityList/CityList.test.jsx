a5c06823fb6ffd3a995d936dd82378a6820b66ba
FX345 <fg1118@nyu.edu>
2026-02-26 08:09:57 +0800

    Fix CityList test

--- src/Cities/CityList/CityList.test.jsx
+++ src/Cities/CityList/CityList.test.jsx
@@ -7,7 +7,11 @@ import CityList from "./CityList";
 vi.mock("../../hooks/useRecord", () => ({
     default: vi.fn(() => {
         return [
-            [],
+            [
+                { _id: "1", name: "New York", state_name: "New York", nation_name: "USA", latitude: 40.7, longitude: -74.0 },
+                { _id: "2", name: "Los Angeles", state_name: "California", nation_name: "USA", latitude: 34.0, longitude: -118.2 },
+                { _id: "3", name: "Chicago", state_name: "Illinois", nation_name: "USA", latitude: 41.9, longitude: -87.6 },
+            ],
             vi.fn(),
             vi.fn(),
         ]

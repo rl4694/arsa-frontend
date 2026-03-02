00f0acc386698a7113ea27a0637160897992bc54
ryljin <ryanjin102354@gmail.com>
2026-03-01 20:35:17 -0800

    added improvements to error handling

--- src/api.js
+++ src/api.js
@@ -17,4 +17,36 @@ const api = axios.create({
     },
 })
 
+// request interceptor
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token")
+        if (token){
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+)
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response){
+            console.error(
+                `API Error ${error.response.status}:`,
+                error.response.data
+            )
+        }
+        else if (error.request){
+            console.error("Network error - Server Unreachable")
+        }
+        else{
+            console.error("Unexpected error:", error.message)
+        }
+
+        return Promise.reject(error)
+    }
+)
+
 export default api
